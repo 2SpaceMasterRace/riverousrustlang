8eb49fe1576a0053337cb29147f46461765979d1
2SpaceMasterRace <alphanovachrono@gmail.com>
2025-10-05 08:43:53 -0400

    [BadStack] Finished 2.3, 2.4, 2.5 - understanding ownership, implementing push and pop functions

--- Learn_Rust_With_Entirely_Too_Many_Linked_Lists/lists/target/debug/deps/lists-2bc11de93c844ccc.d
+++ Learn_Rust_With_Entirely_Too_Many_Linked_Lists/lists/target/debug/deps/lists-2bc11de93c844ccc.d
@@ -1,8 +1,8 @@
-/home/nanodijkstra/Projects/riverousrustlang/Learn_Rust_With_Entirely_Too_Many_Linked_Lists/lists/target/debug/deps/lists-2bc11de93c844ccc.d: src/lib.rs src/first.rs
+/home/nanodijkstra/Work/Projects/riverousrustlang/Learn_Rust_With_Entirely_Too_Many_Linked_Lists/lists/target/debug/deps/lists-2bc11de93c844ccc.d: src/lib.rs src/first.rs
 
-/home/nanodijkstra/Projects/riverousrustlang/Learn_Rust_With_Entirely_Too_Many_Linked_Lists/lists/target/debug/deps/liblists-2bc11de93c844ccc.rlib: src/lib.rs src/first.rs
+/home/nanodijkstra/Work/Projects/riverousrustlang/Learn_Rust_With_Entirely_Too_Many_Linked_Lists/lists/target/debug/deps/liblists-2bc11de93c844ccc.rlib: src/lib.rs src/first.rs
 
-/home/nanodijkstra/Projects/riverousrustlang/Learn_Rust_With_Entirely_Too_Many_Linked_Lists/lists/target/debug/deps/liblists-2bc11de93c844ccc.rmeta: src/lib.rs src/first.rs
+/home/nanodijkstra/Work/Projects/riverousrustlang/Learn_Rust_With_Entirely_Too_Many_Linked_Lists/lists/target/debug/deps/liblists-2bc11de93c844ccc.rmeta: src/lib.rs src/first.rs
 
 src/lib.rs:
 src/first.rs:
